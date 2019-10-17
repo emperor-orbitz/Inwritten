1a8ef7cc16ede58821540f009d1664f4c1065b8d
Abdulgaffar Abdulmalik <malorbit360@gmail.com>
2019-10-17 22:09:39 +0100

    One more push before Initial Deploy

--- Server/Database/db.js
+++ Server/Database/db.js
@@ -1,7 +1,10 @@
 var mongoose = require('mongoose');
 
 
-var conn = mongoose.connect('mongodb://localhost:27017/penbox', { useNewUrlParser: true, useCreateIndex:true });
+var conn = mongoose.connect('mongodb+srv://malik:malik12345@cluster0-lhdgx.azure.mongodb.net/test?retryWrites=true&w=majority', 
+{ useNewUrlParser: true, useCreateIndex:true,
+  useUnifiedTopology: true 
+});
 conn.then((message) => {
  
   console.log(`successful database connection!!`);
