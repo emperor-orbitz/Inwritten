0fd13bde19102a8a18cc58466d1582c3a06df916
Abdulgaffar Abdulmalik <malorbit360@gmail.com>
2019-10-17 22:31:48 +0100

    Work

--- Server/server.js
+++ Server/server.js
@@ -13,7 +13,7 @@ require("dotenv").config({path:__dirname +"/.env"});
 require('./Utils/passport');
 require("./Database/db");
 
-var port = process.env.API_PORT;
+var port = process.env.PORT || 5000;
 
 var route_config = require('./Utils/route_config');
 
