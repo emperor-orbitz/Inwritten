14417cf768c577b3549186f7b59a35dbd128e96e
Abdulgaffar <malorbit360@gmail.com>
2020-06-17 10:49:27 -0700

    changesd launchpage

--- Client/assets/js/launch.js
+++ Client/assets/js/launch.js
@@ -1,7 +1,7 @@
 
 
 $(document).ready(function(ev){
-var arr =["thoughts", "ideas" , "stories", "contents", "articles"];
+var arr =["THOUGHTS", "IDEAS" , "STORIES", "CONTENTS", "ARTICLES"];
 let i =0;
 
 setInterval(()=>{
